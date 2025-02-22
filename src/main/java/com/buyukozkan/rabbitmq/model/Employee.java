634c3e57e7997cfb270c91acc685cfa71a2874b4
halukbuyukozkan <halukbuyukozkan@gmail.com>
2025-02-22 14:42:02 +0300

    first commit

---
+++ src/main/java/com/buyukozkan/rabbitmq/model/Employee.java
@@ -0,0 +1,30 @@
+package com.buyukozkan.rabbitmq.model;
+
+public class Employee {
+
+    private String id;
+    private String name;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + "]";
+    }
+
+}
