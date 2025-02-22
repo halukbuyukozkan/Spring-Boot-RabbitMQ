f99b59bb8b7c98f3cb82f745eafb752baaf5a0e3
halukbuyukozkan <halukbuyukozkan@gmail.com>
2025-02-22 15:00:27 +0300

    Enhance RabbitMQ configuration with dynamic queue, exchange, and routing key setup

--- src/main/java/com/buyukozkan/rabbitmq/config/RabbitMQConfig.java
+++ src/main/java/com/buyukozkan/rabbitmq/config/RabbitMQConfig.java
@@ -1,13 +1,50 @@
 package com.buyukozkan.rabbitmq.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${buyukozkan.rabbitmq.queue}")
+    String queueName;
+
+    @Value("${buyukozkan.rabbitmq.exchange}")
+    String exchange = "employee_exchange";
+
+    @Value("${buyukozkan.rabbitmq.routingkey}")
+    private String routingKey = "employee_routingKey";
+
     @Bean
-    public Queue queue() {
-        return new Queue("myQueue", false);
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 }
